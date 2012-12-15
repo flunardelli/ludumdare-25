b3d6c605dafd07633c5ee85c8a8e8beb90074be6
guilhermescosta <guilhermescosta@dcd21d61-212b-d821-4b5b-779ab5956b1f>
2012-12-15 04:43:50 +0000

    First Gameplay test

---
+++ trunk/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	
+	private float x;
+	private float y;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		y =  Input.GetAxis("Vertical")* Time.deltaTime * speed;
+		x = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+		
+		transform.Translate(x, y, 0);
+	
+	}
+}
