915a7160ca390327baab08241f7a4b43bee6f135
calvinsm@gmail.com <calvinsm@gmail.com@dcd21d61-212b-d821-4b5b-779ab5956b1f>
2012-12-16 16:06:23 +0000

    temporizador Tiro

--- trunk/Assets/Scripts/ShootScript.cs
+++ trunk/Assets/Scripts/ShootScript.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class ShootScript : MonoBehaviour {
 	
-	public Rigidbody bullet;
-	public float power = 100f;
 
+	public float power = 100f;
+	public float m_timer = 0.6f, staytimer;
+	public GameObject bullet;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,13 +14,12 @@ public class ShootScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+		m_timer -= Time.deltaTime;
+		if (Input.GetButtonDown("Fire1") && m_timer < 0) {
 			Rigidbody clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
-			clone.velocity = -transform.up * power;
-			Destroy(clone.gameObject,2);			
-		}
-		
-				
-		
+			//clone.velocity -= transform.up * power;
+			//Destroy(clone.gameObject,2);			
+			m_timer = staytimer;
+		}		
 	}
 }
